30077b098eda55ea60deafac34c2eef7df088a16
Boston M. <69646049+boston2029@users.noreply.github.com>
2024-07-19 12:02:53 -0500

    Update game.js

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ const player = "p"
 const wall = "w"
 const portal = "n"
 const pushable = "b"
+const altchoice = "c"
 
 setLegend(
   [wall, bitmap`
@@ -81,6 +82,23 @@ setLegend(
 ...4744774474...
 ...4774444774...
 ...4444444444...
+................`],
+  [altchoice, bitmap`
+................
+...HHHHHHHHHH...
+...H99999999H...
+..H999HHHH999H..
+..H99HHHHHH99H..
+..H99HH99HH99H..
+..H99HH99HH99H..
+..H99HH99HH99H..
+..H99HH99HH99H..
+..H99HHHHHH99H..
+..H99HHHHHH99H..
+..HH9H9999H9HH..
+...H9HH99HH9H...
+...H99HHHH99H...
+...HHHHHHHHHH...
 ................`]
 )
 
@@ -123,7 +141,89 @@ wwwwbwwwwwb
 ww...wwwww.
 ww.n.wwwww.
 ww...wwwwww
-wwwwwwwwwww`
+wwwwwwwwwww`,
+  map`
+n..........
+.bbbbbbbbbb
+.bbbbbbbbbb
+.bbbbbbbbbb
+.bbbbbbbbbb
+.bbbbbbbbbb
+.bbbbbbbbbb
+.bbbbbbbbbp`,
+  map`
+....wnw....
+wwwww.wwwww
+wwwww.wwwww
+wwwww.wwwww
+wwwww.wwwww
+wwwww.wwwww
+wwwww.wwwww
+wwwwwpwwwww`,
+  map`
+.wwwwnwwww.
+.wwww.wwww.
+.wwww.wwww.
+.wwww.wwww.
+.wwww.wwww.
+.wwww.wwww.
+.wwww.wwww.
+.wwwwpwwww.`,
+  map`
+..wwwnwww..
+..www.www..
+..www.www..
+..www.www..
+..www.www..
+..www.www..
+..www.www..
+..wwwpwww..`,
+  map`
+....wnw....
+....w.w....
+....w.w....
+....w.w....
+....w.w....
+....w.w....
+....w.w....
+....wpw....`,
+  map`
+....wn.....
+......w....
+....w......
+......w....
+....w......
+......w....
+....w......
+.....pw....`,
+  map`
+n..........
+...........
+...........
+...........
+...........
+...........
+...........
+..........p`,
+  map`
+bb.wwwwwwww
+bnbw.....cw
+.bbw..wwwww
+ww....wwwww
+wwwww.wwwww
+wwwww.wwwww
+wwwww.wwwww
+w....p....w`,
+  map`
+...b..b....
+b.....b.b.b
+..b.b......
+.b........b
+..bb..b....
+....b......
+p......b..b
+....b......
+b.b...b....`
 ]
 
 setMap(levels[level])
@@ -150,10 +250,31 @@ onInput("s", () => {
 onInput("d", () => {
   getFirst(player).x += 1
 })
+onInput("l", () => {
+  level=0
+  clearText()
+  addText("where am i?", {
+    x: 4,
+    y: 14
+  })
+  setMap(levels[level])
+})
 
 afterInput(() => {
   const playerTile = getTile(getFirst(player).x, getFirst(player).y)
   const hasReachedPortal = playerTile.some(sprite => sprite.type === portal)
+  const hasReachedAlt = playerTile.some(sprite => sprite.type === altchoice)
+  if (hasReachedAlt) {
+    setMap(map`
+bbb
+bpb
+bbb`)
+    clearText()
+    addText("you lost", {color: color`2`, y:0})
+    addText("evil guy got you", {color: color`2`, y:2})
+    addText("press l to replay!", {color: color`2`, y:13})
+    return
+  }
   if (hasReachedPortal) {
     clearText()
     level++
@@ -170,6 +291,37 @@ afterInput(() => {
           x: 1,
           y: 1
         })
+        break
+      case 5:
+        addText("NONONONO TURN BACK", {
+          x: 1,
+          y: 1
+        })
+        break
+      case 6:
+        addText("THISWAY", {
+          x: 0,
+          y: 1
+        })
+        addText("TRUST", {
+          x: 15,
+          y: 1
+        })
+        break
+      case 11:
+        addText("run quick", {
+          x: 3,
+          y: 7
+        })
+        break
+      case 12:
+        addText("who do you choose", {y:12, color: color`1`})
+        break
+      case 13:
+        addText("you won!", {color: color`0`, y:0})
+        addText("press l to replay!", {color: color`0`, y:2})
+        addText("or mess with", {color: color`0`, y:6})
+        addText("these pushables!", {color: color`0`, y:7})
         break
     }
   }
